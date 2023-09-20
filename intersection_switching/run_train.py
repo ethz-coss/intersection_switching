9e060f6d18ce800a5e7f1291e602030b106b6371
Marcin Korecki <mkorecki@eu-login-14.euler.ethz.ch>
2023-09-20 13:06:02 +0200

    added two new scenarios

--- intersection_switching/run_train.py
+++ intersection_switching/run_train.py
@@ -1,26 +1,37 @@
-
 import os
+from shutil import which
 
-low_balanced = [11, 11]
-low_unbalanced = [11, 6]
+IS_SLURM = which('sbatch') is not None
 
-medium_balanced = [22, 22]
-medium_unbalanced = [22, 11]
-
-high_balanced = [32, 32]
-high_unbalanced = [32, 16]
-
-
-traffic_conditions = [low_balanced, low_unbalanced, medium_balanced, medium_unbalanced, high_balanced, high_unbalanced]
 # reward_types = ["stops", "speed", "wait"]
-reward_types = ['both']
+reward_types = ['localwait', "unique_stops"]
+sim_steps = 3600
+episodes = 400
 
-for reward in reward_types:
-    for traffic in traffic_conditions:
-        os.system("python runner.py --sim_config '../scenarios/loop_intersection/rings.config' --num_sim_steps 3600 --eps_start 1 --lr 0.0005 --mode train --agents_type learning --num_episodes 150 --replay True --mfd False --reward_type " + reward + " --n_vehs " + str(traffic[0]) + " " + str(traffic[1]))
+configs = ['../scenarios/hangzhou/1.config',
+           '../scenarios/hangzhou/2.config',
+           '../scenarios/ny16/1.config',
+           '../scenarios/atlanta/1.config',
+           '../scenarios/la/1.config'
+          ]
+
+learning_rates = {'stops': 0.005,'unique_stops': 0.005,
+                  'wait': 0.05,
+                  'localwait': 0.05}
+decays = {'stops': 0.00005,
+            'unique_stops': 0.00005,
+                  'wait': 0.00005,
+                  'localwait': 0.00005}
+for config in configs:
+    for reward in reward_types:
+        # os.system("python runner.py --sim_config '../scenarios/loop_intersection/rings.config' --num_sim_steps 3600 --eps_start 1 --lr 0.0005 --mode train --agents_type learning --num_episodes 150 --replay True --mfd False --reward_type " + reward + " --n_vehs " + str(traffic[0]) + " " + str(traffic[1]))
 
 # 'python runner.py --sim_config ../scenarios/2x2/1.config --num_sim_steps 3600 --eps_start 1 --lr 0.0005 --mode train --agents_type learning --num_episodes 100 --replay True --mfd False --reward_type stops'
 
-        # os.system("sbatch -n 8 --time=8:00:00 --wrap \"python runner.py --sim_config '../scenarios/loop_intersection/rings.config' --num_sim_steps 3600 --eps_start 1 --lr 0.0005 --mode train --agents_type learning --num_episodes 150 --replay True --mfd False --reward_type " + reward + " --n_vehs " + str(traffic[0]) + " " + str(traffic[1]) + "\"" )
-        
-sbatch -n 8 --time=8:00:00 --wrap "python runner.py --sim_config ../scenarios/hangzhou/1.config --num_sim_steps 3600 --eps_start 1 --lr 0.0005 --mode train --agents_type learning --num_episodes 150 --replay False --mfd False --reward_type wait"
+        pycalls = f"python runner.py --sim_config {config} --num_sim_steps {sim_steps} --eps_start 1 --eps_decay {decays[reward]} --lr {learning_rates[reward]} --mode train --agents_type learning --num_episodes {episodes} --mfd False --reward_type {reward}"
+        if IS_SLURM:
+            os.system(f"sbatch -n 4  --mem-per-cpu=4G --time=9:00:00 --wrap '{pycalls}'")
+        else:
+            os.system(f'{pycalls} &')
+    #     break
+    # break
